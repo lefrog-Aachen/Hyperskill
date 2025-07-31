758a16b5e34354d427af6e4aad617ad82b79c16b
Remi Butaud <remi@incirt.de>
2025-07-31 17:35:54 +0200

    Stage 2 version - can be improved by factoring

--- Sorting Tool with Python/task/main.py
+++ Sorting Tool with Python/task/main.py
@@ -1,16 +1,50 @@
 import argparse
+from math import floor
 parser = argparse.ArgumentParser()
-parser.parse_args()
+parser.add_argument("-dataType", type=str, choices= ['long', 'line', 'word'], default='word')
+args = parser.parse_args()
+# if args.dataType == 'word':
+#     print('choice is word')
+inputs = []
+while True:
+    try:
+        if args.dataType == 'line':
+            data = [input().rstrip()]
+        else:
+            data = input().split()
+        inputs.extend(data)
+    except EOFError:
+        break
 
-int_list = []
-# while True:
-#     try:
-#         data = input().split()
-#         int_list.extend(data)
-#     except EOFError:
-#         break
+if args.dataType == 'line':
+    l_count = len(inputs)
+    print(f'Total lines: {l_count}.')
+    print(f'The longest line:')
+    l_max = 0
+    longest_line = None
+    for l in inputs:
+        if len(l) > l_max:
+            l_max = len(l)
+            longest_line = l
+    print(longest_line)
+    n_l = inputs.count(longest_line)
+    print(f'({n_l} time(s), {floor(100*n_l/l_count)}%).')
 
-print(f'Total numbers: {len(int_list)}.')
-# z = max(int_list)
-z = 1
-print(f'The greatest number: {z} ({int_list.count(z)} time(s)).')
+if args.dataType == 'word':
+    l_count = len(inputs)
+    print(f'Total words: {l_count}.')
+    l_max = 0
+    longest_word = None
+    for l in inputs:
+        if len(l) > l_max:
+            l_max = len(l)
+            longest_word = l
+    n_l = inputs.count(longest_word)
+    print(f'The longest word: {longest_word} ({n_l} time(s), {floor(100*n_l/l_count)}%).')
+
+if args.dataType == 'long':
+    l_count = len(inputs)
+    print(f'Total numbers: {l_count}.')
+    l_max = max(inputs)
+    n_l = inputs.count(l_max)
+    print(f'The greatest number: {l_max} ({n_l} time(s)), {floor(100*n_l/l_count)}%.')
