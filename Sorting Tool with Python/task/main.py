c6155f6b59182e0f9468b2639fa02268c977a9cc
Remi Butaud <remi@incirt.de>
2025-08-03 23:04:56 +0200

    Stage 4 version ok debug functions removed

--- Sorting Tool with Python/task/main.py
+++ Sorting Tool with Python/task/main.py
@@ -22,12 +22,9 @@ class Element:
         self.counted_elements = {}
 
     def do_sort(self):
-        # if self.sorting == 'natural':
         self.sorted_elements = sorted(self.elements)
-        # elif self.sorting == 'byCount':
         self.counted_elements = Counter(self.elements).most_common()
         self.counted_elements.sort(key=lambda x: (x[1], x[0]))
-        # print(self.counted_elements)
 
     def print_elements(self):
         print(f'Total {self.TEXT_HEAD[self.nature]}: {self.n_elements}.')
@@ -40,15 +37,8 @@ class Element:
                 print('Sorted data:', *self.sorted_elements)
         elif self.sorting == 'byCount':
             for i in range(len(self.counted_elements)):
-            # for item in sorted(self.counted_elements):
-            # for e in self.counted_elements.keys():
-            #     print(self.counted_elements)
                 e, c = self.counted_elements[i]
-                # print(e, c)
-                # c = self.counted_elements[e]
                 print(f'{e}: {c} time(s), {floor(100 * c / self.n_elements)}%')
-            # for e, c in self.sorted_elements:
-            #     print(f'{e}: {c} time(s), {floor(100 * c / self.n_elements)}%')
 
 # Build the lists, according to their type (as defined by -dataType)
 # based on the user inputs
@@ -70,35 +60,3 @@ while True:
 element = Element(inputs, data_type, sort_type)
 element.do_sort()
 element.print_elements()
-
-# 
-# def do_count_size(elements, el_type):
-#     l_cnt = len(elements)
-#     max_val = 0
-#     longest = None
-#     if el_type == 'long':
-#         longest = max(elements)
-#     else:
-#         for l in elements:
-#             if len(l) > max_val:
-#                 max_val = len(l)
-#                 longest = l
-#     num = inputs.count(longest)
-#     return l_cnt, longest, num
-# 
-# text = {'long': ['greatest','number'],
-#         'word': ['longest','word'],
-#         'line': ['longest','line']}
-# 
-# cnt, longest, num = do_count_size(inputs, n_type)
-# 
-# print(f'Total {text[n_type][1]}s: {cnt}.')
-# if do_sort:
-#     s_inputs = sorted(inputs, key=lambda x: int(x))
-#     print('Sorted data:',*s_inputs)
-# elif n_type == 'line':
-#     print('The longest line:')
-#     print(longest)
-#     print(f'({num} time(s), {floor( 100 * num / cnt)}%).')
-# else:
-#     print(f'The {text[n_type][0]} {text[n_type][1]}: {longest} ({num} time(s), {floor( 100 * num / cnt)}%).')
