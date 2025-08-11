45da3558ab3407d3ffebe74124cd9efc72faf3db
Remi Butaud <remi@incirt.de>
2025-08-11 14:27:46 +0200

    Stage 5 version with more arguments. Tests pass.

--- Sorting Tool with Python/task/main.py
+++ Sorting Tool with Python/task/main.py
@@ -3,13 +3,27 @@ from math import floor
 from collections import Counter
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-dataType", type=str, choices= ['long', 'line', 'word'], default='word')
-parser.add_argument("-sortingType", type=str, choices= ['natural', 'byCount'], default='natural')
-args = parser.parse_args()
+# parser.add_argument("-dataType", type=str, choices= ['long', 'line', 'word'], default='word')
+# parser.add_argument("-sortingType", type=str, choices= ['natural', 'byCount'], default='natural')
+#parser.add_argument("input_str", type=str, nargs='*')
+parser.add_argument("-dataType", type=str, nargs='?', const='void', default='word')
+parser.add_argument("-sortingType", type=str, nargs='?', const='void', default='natural')
+# parser.add_argument("arg_list", type=str, nargs='*')
+args = parser.parse_known_args()
 inputs = []
+do_exit = False
+sort_type = args[0].sortingType
+if sort_type not in ['natural', 'byCount']:
+    print('No sorting type defined!')
+    do_exit = True
+data_type = args[0].dataType
+if data_type not in ['long', 'line', 'word']:
+    print('No data type defined!')
+    do_exit = True
 
-sort_type = args.sortingType
-data_type = args.dataType
+for extra_arg in args[1]:
+    # if extra_arg.startswith('-'):
+    print(f'{extra_arg} is not a valid parameter. It will be skipped.')
 
 class Element:
     TEXT_HEAD = {'long': 'numbers', 'word': 'words', 'line': 'lines'}
@@ -42,21 +56,28 @@ class Element:
 
 # Build the lists, according to their type (as defined by -dataType)
 # based on the user inputs
-while True:
-    try:
-        if data_type == 'line':
-            # read the lines as a single element each
-            data = [input().rstrip()]
-        elif data_type == 'word':
-            # read as string elements
-            data = input().split()
-        else:
-            # read as integer
-            data = [int(n) for n in input().split()]
-        inputs.extend(data)
-    except EOFError:
-        break
+if not do_exit:
+    while True:
+        try:
+            if data_type == 'line':
+                # read the lines as a single element each
+                data = [input().rstrip()]
+            # elif data_type == 'word':
+            else:
+                # read as string elements
+                data = input().split()
+                if data_type == 'long':
+                    data_clean = []
+                    for ix in range(len(data)):
+                        try:
+                            data_clean.append(int(data[ix]))
+                        except ValueError:
+                            print(f'{data[ix]} is not a long. It will be skipped.')
+                    data = data_clean
+            inputs.extend(data)
+        except EOFError:
+            break
 
-element = Element(inputs, data_type, sort_type)
-element.do_sort()
-element.print_elements()
+    element = Element(inputs, data_type, sort_type)
+    element.do_sort()
+    element.print_elements()
